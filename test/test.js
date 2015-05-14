58860e909260502ae4470d947f12b4872bf77efa
Peter Braun <peter-braun@gmx.net>
2015-05-14 23:54:05 +0200

    fixed tests

--- test/test.js
+++ test/test.js
@@ -4,18 +4,14 @@ var Plugin = require("../index");
 describe("Typescript compilation", function () {
   describe("basic compilation", function () {
     it("should be able to compile strings", function () {
-      assert.doesNotThrow(function () {
-        new Plugin().compile("module Navy { export class Ship { isSunk: boolean; } }", null, function () {
-          // dummy callback
-        });
+      new Plugin().compile("module Navy { export class Ship { isSunk: boolean; } }", null, function (error) {
+        assert.equal(error, null);
       });
     });
 
     it("should halt on errors", function () {
-      assert.throws(function () {
-        new Plugin.compile("nonsense", null, function () {
-          // dummy callback
-        });
+      new Plugin().compile("classs A {}", null, function (error) {
+        assert.notEqual(error, null);
       });
     });
   })
