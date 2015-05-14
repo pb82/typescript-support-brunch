134bb52e1a0395e42587618ccdefd6b6dbe130f3
Peter Braun <peter-braun@gmx.net>
2015-05-14 23:09:33 +0200

    support config: tscArgs

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var tsc = require('typescript-compiler');
 
 var TypescriptCompiler = function (config) {
   // TODO: support for compiler options
+  if (config && config.plugins) {
+    this.config = config.plugins.Typescript;
+  } else {
+    this.config = {};
+  }
 };
 
 TypescriptCompiler.prototype.brunchPlugin = true;
@@ -9,7 +14,7 @@ TypescriptCompiler.prototype.type = 'javascript';
 TypescriptCompiler.prototype.extension = "ts";
 
 TypescriptCompiler.prototype.compile = function (data, path, callback) {
-  var compiled = tsc.compileString(data, null, null, function (error) {
+  var compiled = tsc.compileString(data, this.config.tscArgs, null, function (error) {
     callback(error.formattedMessage);
   });
 
